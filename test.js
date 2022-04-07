73894d5c4df9c22f17e9ad9d3afb11eae3793204
demons1502 <nnhau.20it2@vku.udn.vn>
2022-04-07 16:28:18 +0700

    first commit

---
+++ test.js
@@ -0,0 +1,17 @@
+function* test() {
+    yield 1;
+
+    console.log('first test');
+
+    yield 2;
+    console.log('second test');
+
+    return 3;
+}
+
+const h = test();
+console.log(h.next());
+console.log(h.next());
+console.log(h.next());
+
+console.log(h);
